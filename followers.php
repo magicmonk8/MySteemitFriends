faeb6151691985f30437d5d15eb28035dd159c26
@magicmonk <Bulletproofmonk@users.noreply.github.com>
2018-01-26 17:58:58 +1000

    Update followers.php
    Database connection codes moved to steemSQLconnect2.php

--- followers.php
+++ followers.php
@@ -137,16 +137,9 @@
 <h3>Select page number</h3><br>
 
 
-
-
-
 <?php
+include 'steemSQLconnect2.php';		
 
-$servername = "sql.steemsql.com:1433";
-
-$username = "steemit";
-
-$password = "steemit";
 
 $numberofpages=7;
 
@@ -238,31 +231,7 @@ echo '<a href="followers.php?page='.($page+1).'" class="btn btn-light" role="but
 
 echo '<form action="followers.php" method="get">Go To Page Number <input type="text" name="page" size="5"> <input type="submit" value="Go"></form><br></div>';
 
-// Use try catch exception handling. Details: https://www.w3schools.com/PhP/php_exception.asp
 
-try {
-
-    // connect to SteemSQL via PDO. Make sure pdo and pdo_dblib extensions are enabled.
-
-    $conn = new PDO("dblib:host=$servername;dbname=DBSteem", $username, $password);
-
-    
-
-    // set the PDO error mode to exception
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    
-
-    // print connection successful message if connection successful.
-
-/*    if ($conn) {
-
-        echo "Connection to database established.<br>";
-
-    }
-
-    */
 
     // test query. Select the name column from the Accounts table where the Id is 29666. Result should be magicmonk.
 
@@ -336,15 +305,7 @@ echo '<thead class="thead-default mobile"><tr><th style="text-align: center;">Ra
 
         echo "</table>";
 
-}
 
-// if cannot connect to database, print error message    
-
-catch(PDOException $e)  {
-
-    echo "Connection failed: " . $e->getMessage();
-
-}
 
 // terminate connectiion
 
