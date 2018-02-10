b498938383a552f76564ece5d5b0c7ef6ce15598
@magicmonk <Bulletproofmonk@users.noreply.github.com>
2018-02-10 22:41:07 +1000

    Update get_esp_rank.php
    updated: use sanitize string function on user input and prepared SQL statement to prevent SQL injection.

--- get_esp_rank.php
+++ get_esp_rank.php
@@ -1,6 +1,9 @@
 <?php
+
+// connect to SteemSQL database
 include 'steemSQLconnect2.php';		
 
+// retrieve global values for calculating Steem Power
 $my_file = fopen("global.txt",'r');
 $total_vesting_fund_steem=fgets($my_file);
 $total_vesting_fund_steem = preg_replace('/[^0-9.]+/', '', $total_vesting_fund_steem);
@@ -8,13 +11,12 @@ $total_vesting_shares=fgets($my_file);
 $total_vesting_shares = preg_replace('/[^0-9.]+/', '', $total_vesting_shares);
 fclose($my_file);
 
-
+// sanitize input for Steemit UserName
 $SteemitUser = $_GET["SteemitUser"];
+$SteemitUser = filter_var($SteemitUser, FILTER_SANITIZE_STRING);
 
-
-    $sql = "
-
-
+// SQL for finding rank of Steemit User for Effective SP
+$sql = "
 ;With cte as
 (
 select effective_vests, RANK() over (order by effective_vests DESC) AS RankByESP, name
@@ -29,25 +31,19 @@ Order by effective_vests DESC
 )
 select effective_vests, RankByESP
 from cte
-where name='".$SteemitUser."'
+where name=:name;
 
 ";
 
     
-
-    // execute the query. Store the results in sth variable.
+// Prepare the SQL query to prevent SQL injection. Bind values to parameters after statement is prepared.
 
     $sth = $conn->prepare($sql);
 
+    $sth -> bindValue(':name', $SteemitUser, PDO::PARAM_STR);
+
     $sth->execute();
 
-    
-
-    $vests;
-
-    $rank;
-
-    
 
     // print the results. If successful, magicmonk will be printed on page.
 
@@ -69,10 +65,6 @@ echo "<p>".$SteemitUser." has ".number_format(round($sp))." effective SP and is 
 $page = ceil($rank/50);
 
 echo "<p><a href=effectiveSP.php?page=".$page."&highlight=".$SteemitUser.">".$SteemitUser." is on page ".$page.". Click to see this page</a>.</p>";
-
-
-
-        
 
 
 
