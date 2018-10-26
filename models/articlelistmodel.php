49182599237ffaa7fc7598f0d5eec0161ac6a928
@magicmonk <Bulletproofmonk@users.noreply.github.com>
2018-10-26 10:19:28 +1000

    Changed SQL to fetch results including / excluding comments
    Depending on the ArticlesOnly value passed onto the model, the model will provide an article list including / excluding comments, for both voted and written article modes.

--- models/articlelistmodel.php
+++ models/articlelistmodel.php
@@ -1,41 +1,40 @@
 <?php
-
-class articlelistmodel {
-	
-	protected $db;
-	
-	public function __construct(PDO $db) {
-		
+class articlelistmodel {	
+	protected $db;        	
+	public function __construct(PDO $db) {		
 		$this->db = $db;
-	}
-	
-	public function gethistory($date,$todate,$voter) {
-		
+	}                                     	
+	public function gethistory($date,$todate,$voter,$articlesonly) {
+    // retrieve list of articles voted on by user		
 		$sql = "SELECT timestamp,weight,author,permlink
 				FROM TxVotes 
 				WHERE (voter=:voter) 
 					AND (timestamp>=Convert(datetime, :date)) 
-					AND (timestamp<=Convert(datetime,:todate)) 
-				ORDER BY timestamp DESC";
-		
-		$result = $this->db->prepare($sql);
-		
+					AND (timestamp<=Convert(datetime,:todate))";
+    // if comments are excluded, add this to SQL
+    if ($articlesonly==2) {
+      $sql.= "AND (permlink NOT LIKE 're-%')";
+    }                                                                                         
+		$sql.="ORDER BY timestamp DESC";                                 		
+		$result = $this->db->prepare($sql);                           		
 		$result -> bindValue(':date', $date, PDO::PARAM_STR);
 		$result -> bindValue(':todate', $todate, PDO::PARAM_STR);
-		$result -> bindValue(':voter', $voter, PDO::PARAM_STR);
-		
-		$result->execute();
-		
+		$result -> bindValue(':voter', $voter, PDO::PARAM_STR);       		
+		$result->execute();                                           		
 		return $result;
-	}
-	
-	public function getwritten($date,$todate,$author) {
+	}             	
+	public function getwritten($date,$todate,$author,$articlesonly) {
+    // retrieve list of articles written by user
 		$sql = "SELECT author,permlink,created AS timestamp
 				FROM Comments 
 				WHERE (author =:author) 
 				AND (created>=Convert(datetime, :date)) 
-				AND (created<=Convert(datetime,:todate))
-				ORDER BY created DESC";
+				AND (created<=Convert(datetime,:todate))";
+    // if comments are excluded, add this to SQL
+    if ($articlesonly==2) {
+      $sql.= "AND (depth=0)";
+    }   
+		$sql.="ORDER BY created DESC";
 		$result = $this->db->prepare($sql);	
 		$result -> bindValue(':date', $date, PDO::PARAM_STR);
 		$result -> bindValue(':todate', $todate, PDO::PARAM_STR);
