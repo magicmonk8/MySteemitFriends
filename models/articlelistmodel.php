bb15cb59d1b96986295ac0a7974e1521481f75e9
@magicmonk <Bulletproofmonk@users.noreply.github.com>
2018-10-30 11:43:51 +1000

    Added tag and title filters to both votes and written articles
    For both Written Articles and Voted Articles queries, the $tag and $title filters from the controller have been used to filter query results. If the user types a tag or a title or both into the form, the result will return articles matching their request.
    The Written Articles query has also been re-written so that it does not rely on the "re-" prefix in the permlink to filter out comments. Rather, the TxVotes table has been joined with the Comments table using the author column as well as the permlink column, to use the depth field in the Comments table to filter out comments.

--- models/articlelistmodel.php
+++ models/articlelistmodel.php
@@ -1,54 +1,68 @@
 <?php
-class articlelistmodel {	
-	protected $db;        	
-	public function __construct(PDO $db) {		
-		$this->db = $db;
-	}                                     	
-	public function gethistory($date,$todate,$voter,$articlesonly) {
-    // retrieve list of articles voted on by user		
-		$sql = "SELECT timestamp,weight,author,permlink
-				FROM TxVotes 
-				WHERE (voter=:voter) 
-					AND (timestamp>=Convert(datetime, :date)) 
-					AND (timestamp<=Convert(datetime,:todate))";
-    // if comments are excluded, add this to SQL
+class articlelistmodel {
+  protected $db;
+  public function __construct(PDO $db) {
+    $this->db = $db;
+  }
+  public function gethistory($date,$todate,$voter,$articlesonly,$tag,$title) {
+    // retrieve list of articles voted on by user
+    $sql = "SELECT timestamp,weight,TxVotes.author,TxVotes.permlink
+            FROM TxVotes LEFT OUTER JOIN Comments
+            ON (TxVotes.author=Comments.author AND TxVotes.permlink=Comments.permlink)
+            WHERE (voter=:voter)
+            AND (timestamp>=Convert(datetime, :date))
+            AND (timestamp<=Convert(datetime,:todate))";
+      // if comments are excluded, add this to SQL
     if ($articlesonly==2) {
-      $sql.= "AND (permlink NOT LIKE 're-%')";
-    }                                                                                         
-		$sql.="ORDER BY timestamp DESC";                                 		
-		$result = $this->db->prepare($sql);                           		
-		$result -> bindValue(':date', $date, PDO::PARAM_STR);
-		$result -> bindValue(':todate', $todate, PDO::PARAM_STR);
-		$result -> bindValue(':voter', $voter, PDO::PARAM_STR);       		
-		$result->execute();                                           		
-		return $result;
-	}             	
-	public function getwritten($date,$todate,$author,$articlesonly) {
+      $sql.= "AND (Comments.depth=0)";
+    }
+      // if there is a tag specified, add this to SQL
+    if ($tag!=NULL) {
+      $sql.="AND (CONTAINS(Comments.json_metadata, '".$tag."'))";
+    }
+    if ($title!=NULL) {
+      $sql.="AND (CONTAINS(Comments.title, '".$title."'))";
+    }
+    $sql.="ORDER BY timestamp DESC";
+    $result = $this->db->prepare($sql);
+    $result -> bindValue(':date', $date, PDO::PARAM_STR);
+    $result -> bindValue(':todate', $todate, PDO::PARAM_STR);
+    $result -> bindValue(':voter', $voter, PDO::PARAM_STR);
+    $result->execute();
+    return $result;
+  }
+  public function getwritten($date,$todate,$author,$articlesonly,$tag,$title) {
     // retrieve list of articles written by user
-		$sql = "SELECT author,permlink,created AS timestamp
-				FROM Comments 
-				WHERE (author =:author) 
-				AND (created>=Convert(datetime, :date)) 
-				AND (created<=Convert(datetime,:todate))";
+    $sql = "SELECT author,permlink,created AS timestamp
+            FROM Comments
+            WHERE (author =:author)
+            AND (created>=Convert(datetime,:date))
+            AND (created<=Convert(datetime,:todate))
+            ";
     // if comments are excluded, add this to SQL
     if ($articlesonly==2) {
       $sql.= "AND (depth=0)";
-    }   
-		$sql.="ORDER BY created DESC";
-		$result = $this->db->prepare($sql);	
-		$result -> bindValue(':date', $date, PDO::PARAM_STR);
-		$result -> bindValue(':todate', $todate, PDO::PARAM_STR);
-		$result -> bindValue(':author', $author, PDO::PARAM_STR);
-		$result->execute();
-		return $result;
-	}
-	
-	public function close_connection() {
-        if(isset($this->db))
-        {
-            $this->db->close();
-            unset($this->db);
-        }
     }
-	
+    // if there is a tag specified, add this to SQL
+    if ($tag!=NULL) {
+      $sql.="AND (CONTAINS(json_metadata, '".$tag."'))";
+    }
+    // if there is a title specified, add this to SQL
+    if ($title!=NULL) {
+      $sql.="AND (CONTAINS(title, '".$title."'))";
+    }
+    $sql.="ORDER BY created DESC";
+    $result = $this->db->prepare($sql); 
+    $result -> bindValue(':date', $date, PDO::PARAM_STR);
+    $result -> bindValue(':todate', $todate, PDO::PARAM_STR);
+    $result -> bindValue(':author', $author, PDO::PARAM_STR);
+    $result->execute();
+    return $result;
+  }
+  public function close_connection() {
+    if(isset($this->db)) {
+      $this->db->close();
+      unset($this->db);
+    }
+  }
 }
