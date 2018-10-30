6f819fc8e0b6f1ccfcb71f0fa5640bffd1072025
@magicmonk <Bulletproofmonk@users.noreply.github.com>
2018-10-30 11:49:16 +1000

    Fixed indentation. See previous commit for changes to this file other than indenting

--- articlelist.php
+++ articlelist.php
@@ -3,32 +3,32 @@
 include 'steemSQLconnect2.php';
 // Make your model available
 include 'models/articlelistmodel.php';
-// Get values from URL	
+// Get values from URL  
 // voter details stripped from URL
 if (isset($_GET["voter"])) {
-	$voter = rtrim($_GET["voter"]);
+  $voter = rtrim($_GET["voter"]);
 }     
-// check if date has been entered (submitted via form)	
+// check if date has been entered (submitted via form)
 if (isset($_GET["date"])) {
-	$date = $_GET["date"];
+  $date = $_GET["date"];
 } else {
-	// set date variable for SQL query.
-	$date = date("Y-m-d", strtotime("-1 months"));
+  // set date variable for SQL query.
+  $date = date("Y-m-d", strtotime("-1 months"));
 }     
 // check if to date has been entered (submitted via form)
 if (isset($_GET["toDate"])) {
-	$todate = $_GET["toDate"];
+  $todate = $_GET["toDate"];
 } else { 
-	$todate = date("Y-m-d", strtotime("+1 day"));	
+  $todate = date("Y-m-d", strtotime("+1 day")); 
 }     
 if (isset($_GET["mode"])) {
-	$mode = $_GET["mode"];
-}           	
-// retrieve choice for whether to include articles only or to include comments as well. 
+  $mode = $_GET["mode"];
+}             
+// retrieve choice for whether to include articles only or to include comments as well.
 if (isset($_GET["Articlesonly"])) {
-	$articlesonly = $_GET["Articlesonly"];
+  $articlesonly = $_GET["Articlesonly"];
 } else {
-	$articlesonly = 1;
+  $articlesonly = 1;
 } 
 // retrieve the tag input box value.
 if (isset($_GET["tag"])) {
@@ -48,9 +48,9 @@ if ($voter&&$mode=="upvote") {
   // get list of results
   $results = $articlelistmodel -> gethistory($date,$todate,$voter,$articlesonly,$tag,$title);
 } elseif ($voter&&$mode=="written") {
-  $results = $articlelistmodel -> getwritten($date,$todate,$voter,$articlesonly,$tag,$title);	
-}                                               
+  $results = $articlelistmodel -> getwritten($date,$todate,$voter,$articlesonly,$tag,$title); 
+}
 // Show the view
-include 'views/articlelistview.php';            
-$articlelistmodel -> close_connection();        
-?>                                              
+include 'views/articlelistview.php';
+$articlelistmodel -> close_connection();
+?>
